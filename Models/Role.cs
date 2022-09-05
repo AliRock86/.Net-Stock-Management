2326c1f9559821d0f514df4be818ca95f8d6a4ee
AliRock86 <alicallofduty86@gmail.com>
2022-09-05 11:23:49 +0300

    first commit

---
+++ Models/Role.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Stock_Management.Models
+{
+    public class Role
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string RoleName { get; set; }
+        public string RoleNameArb { get; set; }
+        public virtual ICollection<User> Users { get; set; }
+    }
+}
+
---
+++ Models/User.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Net.Sockets;
+using System.Security;
+
+namespace Stock_Management.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [Column(TypeName = "bigint")]
+        public int Phone { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string Salt { get; set; }
+        public bool Active { get; set; } = true;
+
+        //[Required]
+        public DateTime? CreatedDate { get; set; }
+
+        //[Required]
+        public DateTime? UpdatedDate { get; set; }
+
+        [Required]
+        public int RoleId { get; set; }
+        public virtual Role Role { get; set; }
+        public int? EmployeeId { get; set; }
+
+        public virtual ICollection<RoleUser> FeedbackTickets { get; set; }
+        public virtual ICollection<PermissionUser> PermissionUsers { get; set; }
+
+
+    }
+}
